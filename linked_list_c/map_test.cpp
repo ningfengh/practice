18f626cc882ea5495a0f2f4476a181282b207ac1
Ningfeng <ningfengh@gmail.com>
2015-08-27 18:35:13 -0700

    new file:   linked_list_c/map_test.cpp

---
+++ linked_list_c/map_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+
+int main(void)
+{
+	map <int,int> mymap;
+	mymap[1]=2;
+	mymap[2]=3;
+	mymap[3]=4;
+	cout<<mymap[3]<<endl;
+	return 1;
+}
