f34850ff76c1b209562f710cc4003574fa559300
Ningfeng Huang <ningfengh@gmail.com>
2015-08-31 15:42:58 -0700

    new file:   228_Summary_Ranges.cpp

---
+++ array/228_Summary_Ranges.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+void PrintVector(vector<int>& in)
+{
+	int i;
+	for (i=0;i<in.size();i++)
+		cout<<in[i]<<" ";
+	cout<<endl;
+}
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        
+    }
+};
+
+
+int main(void)
+{
+	int rowIndex = 0,i;
+	int a[]={2,2,1,1,1,2,2};
+	int m,n;
+	Solution mysolution;
+	vector<int>  vec_a(a,a+7);
+	
+
+}
