eb420c80dea0d2aa3655a15e7ec13d18c8ff39d1
Ningfeng Huang <ningfengh@gmail.com>
2015-08-31 17:17:11 -0700

    modified:   ../array/79_Word_Search.cpp

--- array/79_Word_Search.cpp
+++ array/79_Word_Search.cpp
@@ -21,17 +21,24 @@ string to_string(int i)
 
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        
+    bool exist(vector<vector<char> >& board, string word) {
+        return false;
     }
 };
 
 
 int main(void)
 {
-
-
+	char board_char[3][4]={{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+	vector<vector<char> > board;
+	Solution mysolution;
+	string word = "ABCC";
+	int i;
+	for (i=0;i<3;i++)
+	{
+		vector<char> tmp(board_char[i],board_char[i]+4);
+		board.push_back(tmp);
+	}
+	cout<<mysolution.exist(board,word)<<endl;	
 	
-	
-
 }
